32ba5573b8c3c855282c8e0fc2760c9852f6f5d2
OrionDB5 <daddobiker@gmail.com>
2017-02-16 17:05:30 +0100

    Updates

--- scripts/scripts/external-monitor.sh
+++ scripts/scripts/external-monitor.sh
@@ -12,7 +12,7 @@ do
          /usr/bin/xrandr --auto && /home/davide/audio-toggle-sink.sh 1
          echo "${dmode}"
     elif [ "${dmode}" = connected ];then
-         xrandr  --output HDMI1 --primary --mode 1920x1080 --pos 1366x0 --rotate normal --output eDP1 --mode 1366x768 --pos 0x0 --rotate normal
+         xrandr  --output HDMI-1 --primary --mode 1920x1080 --pos 1366x0 --rotate normal --output eDP-1 --mode 1366x768 --pos 0x0 --rotate normal
     /home/davide/audio-toggle-sink.sh 0 && killall compton && compton -b
          echo "${dmode}"
     else /usr/bin/xrandr --auto && /home/davide/audio-toggle-sink.sh 1 && killall compton && compton -b
